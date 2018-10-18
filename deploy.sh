67c09fae4b612c691b76ba939a4470c0bc78f34e
Chris Keller <kellerch@umich.edu>
2018-10-18 16:15:36 -0400

    Updated deploy script to update database, update entities, and rebuild cache

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,15 @@
 
 # Pulls code from provided branch name.
 git pull origin $1
+
+# Switch to PHP 7.
+source /opt/rh/rh-php70/enable
+
+# Run any database updates.
+/afs/umich.edu/group/itd/umweb/bin/drush-8.x/drush updatedb -y
+
+# Rebuild entities.
+/afs/umich.edu/group/itd/umweb/bin/drush-8.x/drush entup -y
+
+# Rebuild cache
+/afs/umich.edu/group/itd/umweb/bin/drush-8.x/drush cr
